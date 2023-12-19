bddde39c623238beb3bc077aee30ab1e32428f77
Sarah123Samara <142015738+Sarah123Samara@users.noreply.github.com>
2023-12-19 12:25:19 +0000

    Add files via upload

---
+++ Flask-SQLAlchemy-Static_v2/app.py
@@ -0,0 +1,27 @@
+from flask import render_template, url_for, request
+from models import db, Pet, app
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/add-pet", methods=("GET", "POST"))
+def add_pet():
+    if request.method == "POST":
+        print(request.form)
+        if "name" in request.form:
+            print(request.form["name"])
+    return render_template("addpet.html")
+
+
+@app.route("/pet")
+def pet():
+    return render_template("pet.html")
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, port=8000, host="127.0.0.1")
